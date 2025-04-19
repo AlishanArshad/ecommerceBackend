3474a3b3c78fd3ed3f18cbef6210fe39165e1ed1
Alishan Arshad <aa9641434@gmail.com>
2025-04-19 18:37:23 +0500

    ecommerce end points with paths

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve HTML files
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/products', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'products.html'));
+});
+
+app.get('/products/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'product-details.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
